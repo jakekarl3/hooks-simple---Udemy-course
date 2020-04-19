4521471c0943200ce848901b60b3f2248af11e5e
Jake Karl <31318360+jakekarl3@users.noreply.github.com>
2020-04-19 16:28:05 -0500

    Rendered list of posts/todos in ResourceList

--- src/components/resourceList/index.js
+++ src/components/resourceList/index.js
@@ -3,23 +3,24 @@ import axios from 'axios';
 
 const ResourceList = ({ resource }) => {
   const [resoureces, setResources] = useState([]);
-  
-  const fetchResource = async (resource) => {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${resource}`
-    );
-    
-    setResources(response.data);
-  }
 
-  useEffect(() => {
-    fetchResource(resource)
-  }, [resource]);
+  useEffect(
+    () => {
+      (async resource => {
+        const response = await axios.get(
+          `https://jsonplaceholder.typicode.com/${resource}`
+        );
+      
+      setResources(response.data);
+      })(resource);
+    },
+    [resource]
+  );
 
   return (
-    <div>
-      {resoureces.length}
-    </div>
+    <ul>
+      {resoureces.map(record => <li key={record.id}>{record.title}</li>)}
+    </ul>
   );
 }
 
