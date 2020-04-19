d9b2be2cc72ccd14fc550571a4ab4ba94db5bdec
Jake Karl <31318360+jakekarl3@users.noreply.github.com>
2020-04-19 16:47:45 -0500

    Moved useResources to separate file, created UserListand added to App

--- src/components/resourceList/index.js
+++ src/components/resourceList/index.js
@@ -1,24 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import useResources from '../useResources';
 
-const useResources = (resource) => {
-  const [resoureces, setResources] = useState([]);
-
-  useEffect(
-    () => {
-      (async resource => {
-        const response = await axios.get(
-          `https://jsonplaceholder.typicode.com/${resource}`
-        );
-      
-      setResources(response.data);
-      })(resource);
-    },
-    [resource]
-  );
-
-  return resoureces;
-};
 
 const ResourceList = ({ resource }) => {
   const resources = useResources(resource);
