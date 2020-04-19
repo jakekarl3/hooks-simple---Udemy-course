b4f5f2c3016569e2b0d0fd536ea878e6eae81a9b
Jake Karl <31318360+jakekarl3@users.noreply.github.com>
2020-04-19 15:51:53 -0500

    Added useState, useEffect to ResourceList

--- src/components/resourceList/index.js
+++ src/components/resourceList/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
-  state = { resources: [] };
+const ResourceList = () => {
+  const [resoureces, setResources] = useState([]);
   
-  async componentDidMount() {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-
+  const fetchResource = async () => {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+    );
+      
     this.setState({ resources: response.data });
   }
 
-  render() {
-    return (
-      <div>
-        {this.state.resources.length}
-      </div>
-    );
-  }
+  return (
+    <div>
+      {resoureces.length}
+    </div>
+  );
 }
 
 export default ResourceList;
